3e1095aa06866d87c25b513d4e76d7f4a1e46a43
Kayla Kingston <kkingston09@gmail.com>
2020-06-06 21:16:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,9 +5,19 @@ class Person
   
   def name 
     @name
-  end 
+  end
+  
+   def job=(job)
+    @job = job
+  end
+  
+  def job 
+    @job
+  end
+  
 end 
 
 beyonce = Person.new 
 beyonce.name = "Beyonce"
 beyonce.instance_variable_set(:@name, "Beyonce") 
+beyonce.job = "Singer"
