7080e4307c54420cca66ab6c1e27e6a4fd115383
Kayla Kingston <kkingston09@gmail.com>
2020-06-06 21:04:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,10 @@ class Dog
   def name=(dog_name)
     @name_of_dog = dog_name
   end
-fido.instance_variable_get(:@name)
+  
 
 end
 
 fido = Dog.new 
 fido.name = "Fido"
+fido.instance_variable_get(:@name)
