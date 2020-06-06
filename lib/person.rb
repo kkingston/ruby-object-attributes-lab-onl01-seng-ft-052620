ff8ae14919bcb7c578193be7cede300386619db3
Kayla Kingston <kkingston09@gmail.com>
2020-06-06 21:14:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
 class Person
   def name=(name_person)
-    @name_of_person = name_person
+    @name = name_person
   end
   
   def name 
-    @name_of_person
+    @name
   end 
 end 
 
 beyonce = Person.new 
 beyonce.name = "Beyonce"
-beyonce.instance_variable_set(:@name_of_person, "Beyonce") 
+beyonce.instance_variable_set(:@name, "Beyonce") 
